f05cbca4251ab03f43ec5196e1c84c4e5d8a7f80
Jonas Gann <jonas.gann@gmail.com>
2019-04-21 02:11:09 +0200

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import { FragmentProvider, Fragment } from './fragmentProvider';
+
+
+export function activate(context: vscode.ExtensionContext)
+{
+	const fragmentProvider = new FragmentProvider();
+	vscode.window.registerTreeDataProvider('fragmentEditor', fragmentProvider);
+	vscode.commands.registerCommand('fragmentEditor.addEntry', () => fragmentProvider.addEntry());
+	vscode.commands.registerCommand('fragmentEditor.editEntry', (fragment: Fragment) => fragmentProvider.editEntry(fragment));
+	vscode.commands.registerCommand('fragmentEditor.deleteEntry', (fragment: Fragment) => fragmentProvider.deleteEntry(fragment));
+
+}
+
+export function deactivate() {}
---
+++ src/fragmentProvider.ts
@@ -0,0 +1,153 @@
+import * as vscode from 'vscode';
+import { resolve } from 'path';
+let fs = require("fs");
+
+export class Fragment extends vscode.TreeItem
+{
+    keywords: string[];
+    code: string;
+    constructor(public readonly label: string)
+    {
+        super(label);
+        this.keywords = [];
+        this.code = "";
+    }
+
+    get description(): string
+    {
+        return "";
+    }
+
+    get tooltip(): string
+    {
+        return this.label + "\n\n" + this.code + "\n\n" + this.keywords;
+    }
+}
+
+export class FragmentProvider implements vscode.TreeDataProvider<Fragment>
+{
+    fragments: Fragment[];
+	private _onDidChangeTreeData: vscode.EventEmitter<Fragment | undefined> = new vscode.EventEmitter<Fragment | undefined>();
+	readonly onDidChangeTreeData: vscode.Event<Fragment | undefined> = this._onDidChangeTreeData.event;
+
+    constructor()
+    {
+        this.fragments = [];
+    }
+
+    getTreeItem(element: Fragment): vscode.TreeItem
+    {
+        return element;
+    }
+
+    getChildren(element?: Fragment): Thenable<Fragment[]>
+    {
+        //this.readFragmentFiles();
+        return Promise.resolve(this.fragments);
+    }
+
+    refresh(): void
+    {
+		this._onDidChangeTreeData.fire();
+	}
+
+    editEntry(fragment: Fragment): void
+    {
+        var fragmentFile = vscode.workspace.openTextDocument("C:/Users/Jonas/Documents/fragment-editor/fragments/"+fragment.label+".txt");
+
+        fragmentFile.then((file) =>
+        {
+            vscode.window.showTextDocument(file);
+        });
+
+    }
+
+    addEntry(): void
+    {
+        var input = vscode.window.showInputBox({prompt: "Input a label for the Fragment"});
+
+        input.then((value) =>
+        {
+            for(var cnt = 0; cnt < this.fragments.length; cnt++)
+            {
+                if(this.fragments[cnt].label === value)
+                {
+                    vscode.window.showErrorMessage("Creation of Fragment Cancelled (Label must be unique)");
+                    return;
+                }
+            }
+            if(value === undefined)
+            {
+                this.refresh();
+                vscode.window.showErrorMessage("Creation of Fragment Cancelled");
+                return;
+            }
+            else if(value === '')
+            {
+                this.refresh();
+                vscode.window.showErrorMessage("Creation of Fragment Cancelled (no empty label allowed)");
+                return;
+            }
+            else
+            {
+                fs.writeFile("C:/Users/Jonas/Documents/fragment-editor/fragments/"+value+".txt", "", function(err: Error)
+                {
+                    if(err)
+                    {
+                        vscode.window.showErrorMessage("File not created")
+                    }
+                    else
+                    {
+                        vscode.window.showInformationMessage("File created");
+                    }
+                });
+                this.fragments.push(new Fragment(String(value)));
+                this.refresh();
+                vscode.window.showInformationMessage("New Fragment Added");
+            }
+        });
+    }
+
+    deleteEntry(fragment: Fragment)
+    {
+        this.fragments = this.fragments.filter(function(element, index, arr)
+        {
+            return fragment.label !== element.label;
+        });
+
+        fs.unlink("C:/Users/Jonas/Documents/fragment-editor/fragments/"+fragment.label+".txt", (err: Error) =>
+        {
+            if(err)
+            {
+                vscode.window.showErrorMessage("File not deleted")
+            }
+            else
+            {
+                vscode.window.showInformationMessage("File deleted");
+            }
+            this.refresh();
+        });
+
+        this.refresh();
+
+        vscode.window.showInformationMessage("Fragment Deleted");
+    }
+
+    readFragmentFiles()
+    {
+        var fragmentsList: Fragment[];
+        fs.readdir("C:/Users/Jonas/Documents/fragment-editor/fragments", (err: Error, files: []) =>
+        {
+            if(err)
+            {
+                vscode.window.showErrorMessage("Unable to scan directory: " + err);
+            } 
+            files.forEach((file) =>
+            {
+                this.fragments.push(new Fragment(file));
+                //vscode.window.showInformationMessage(file);
+            });
+            this.fragments = fragmentsList;
+        });
+    }
+}
