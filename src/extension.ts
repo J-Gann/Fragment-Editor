178d39cbe3d5bfe07ae0bb4a942d5b3d9439c646
Julian König <j.koenig97@web.de>
2019-06-24 16:05:51 +0200

    frag.extract fix

--- src/extension.ts
+++ src/extension.ts
@@ -5,14 +5,52 @@ import {FragmentProvider} from './fragmentProvider';
 import {Database} from './database';
 import {TreeItem} from './treeItem';
 import * as path from "path";
+const shell = require("shelljs");
+import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
     Database.setDefaultPath(path.join(context.extensionPath, "data"));
+    
+    // install process upon first activation
+	if (!fs.existsSync(context.globalStoragePath)) {
+		fs.mkdirSync(context.globalStoragePath);
+	}
+	if (!fs.existsSync(context.extensionPath + "/out/frag.extract.host/extract.py")) {
+		fs.readFile(context.extensionPath + "/out/frag.extract.host/raw.py", 'utf8', function (err, data) {
+			if (err) {
+				return console.log(err);
+			}
+			var result = data.replace(/path/, Database.getDefaultPath().replace(/\\/g, '/') + "/fragments.db");
+
+			var path = context.extensionPath + "/out/frag.extract.host/extract.py";
+
+			fs.writeFile(path, result, 'utf8', function (err) {
+				fs.chmod(path, 0o0755, function (err) {
+					if (err) {
+						return console.log(err);
+					}
+				});
+			});
+		});
+		if (process.platform === 'win32') {
+			shell.exec('"' + context.extensionPath + '"' + '\\out\\frag.extract.host\\install_host.bat');
+		} else {
+			shell.exec(context.extensionPath + '/out/frag.extract.host/install_host.sh');
+		}
+	}
+
+
     const fragmentProvider = new FragmentProvider(context);
     var treeView = vscode.window.createTreeView('fragmentEditor', {treeDataProvider: fragmentProvider});
     vscode.commands.registerCommand('fragmentEditor.addFragment', () => fragmentProvider.addFragment());
     vscode.commands.registerCommand('fragmentEditor.editFragment', (treeItem: TreeItem) => fragmentProvider.editFragment(treeItem));
     vscode.commands.registerCommand('fragmentEditor.deleteTreeItem', (treeItem: TreeItem) => fragmentProvider.deleteTreeItem(treeItem));
+
+    // refreshes the Fragmentlist everytime a change in the database is detected (5 sec intervall)
+	fs.watchFile(Database.getDefaultPath() + '/fragments.db', (curr, prev) => {
+		Database.getInstance().loadFragments();
+		fragmentProvider.refresh();
+	});
 }
 
 export function deactivate() {
